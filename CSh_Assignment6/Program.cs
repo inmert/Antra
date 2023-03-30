9baecad7b57e03c25eb9d1d58b37a409aea3db4f
Priyan Rai <raipy@mail.uc.edu>
2023-03-30 15:59:36 +0530

    Update

---
+++ CSh_Assignment6/Program.cs
@@ -0,0 +1,45 @@
+﻿// Assignent 6 Priyan Rai
+// LeetCode Solutions
+
+// Question 15
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        List<IList<int>> output = new List<IList<int>>();
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1, k = nums.Length - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0)
+                {
+                    j++;
+                }
+                    
+                else if (sum > 0)
+                {
+                    k--;
+                }
+                    
+                else
+                {
+                    output.Add(new int[] { nums[i], nums[j], nums[k] });
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return output;
+    }
+}
+
